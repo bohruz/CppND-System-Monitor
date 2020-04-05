c4d6fcf58fbcc5b2ede6fed82d8757d46c60efe0
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-05 19:17:48 -0300

    System.cpp completed

--- src/system.cpp
+++ src/system.cpp
@@ -20,7 +20,13 @@ using std::vector;
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+  vector<int> pids = LinuxParser::Pids();
+  for (auto process : pids) {
+    processes_.push_back(Process(process));
+  }
+  return processes_;
+}
 
 // TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
